969bc167e704737f2ae231456b2748e0d716bacb
ishfaq6308 <ishfaqrather6308@.com>
2023-05-19 16:08:24 +0530

    ishfaq index.php updated

---
+++ index.php
@@ -0,0 +1,177 @@
+<?php
+
+
+
+$insert='false';
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "notesboard";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+  if($_SERVER['REQUEST_METHOD'] == 'POST')
+  {
+    $title = $_POST["title"];
+    $description = $_POST["description"];
+
+    // $sql = "INSERT INTO 'notes' ( 'title', 'descrption')
+    // VALUES (  '$title','$description')";
+    $sql = "INSERT INTO notes (title,  description)
+    VALUES ('$title ', ' $description')";
+    
+
+ 
+
+if (mysqli_query($conn, $sql)) {
+    
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+  }
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>crud</title>
+  
+   
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css" href="style.css"> 
+    <!-- <script src="https://code.jquery.com/jquery-3.7.0.js" integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM=" crossorigin="anonymous"></script> -->
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+</head>
+  <body>
+  
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">ishfaq
+
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link ">Disabled</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+<?php
+
+if($insert)
+
+{
+    echo "<div class='alert alert-success alert-dismissible fade show' role='alert'>
+    <strong>success</strong> Your data has been inserted successfully.
+    <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+  </div>";
+    }
+?>
+    
+    <div class="container my-3">
+        <form action="/CRUD/index.php/" method="POST">
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Title</label>
+    <input type="text" class="form-control" id="title" name="title" aria-describedby="emailHelp">
+    
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">description</label>
+  <textarea class="form-control" placeholder="Leave a comment here" id="description" name="description" style="height: 100px"></textarea>
+  </div>
+ 
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+    </div>
+    <div class="container my-4">
+
+
+ <table class="table"  id="myTable">
+  <thead>
+    <tr>
+      <th scope="col">S.no</th>
+      <th scope="col">Title</th>
+      <th scope="col">Description</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+  <?php
+$sql = "SELECT * FROM notes";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) 
+  {
+    echo " <tr>
+    <td>".$row['s.no']."</td>
+    <td>".$row['title']."</td>
+    <td>".$row['description']."</td>
+    <td>hh</td>
+  </tr>";
+    
+  }
+} 
+else {
+  echo "0 results";
+}
+?>
+   
+  
+  </tbody>
+</table>
+    </div>
+    <hr>
+    <!-- <script src="https://code.jquery.com/jquery-3.7.0.js" integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM=" crossorigin="anonymous"></script> -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+   <!-- <script>
+    let table = new DataTable('#myTable');
+    </script> -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+<!-- <scrit src="//cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script> -->
+<script src="//cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script> 
+    
+    <script>
+    //       $(document).ready( function(){
+    // $('#mytable').DataTable();
+    // });
+    let table = new DataTable('#myTable');
+    </script>
+
+</body>
+</html>
