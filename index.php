e43a16a7ac5bfd9ea9f7f01037f7b8fb7df450b6
ishfaq6308 <ishfaq.jk.technosoft@gmail.com>
2023-05-20 15:17:13 +0530

    links added

--- index.php
+++ index.php
@@ -68,6 +68,9 @@ if (mysqli_query($conn, $sql)) {
         <li class="nav-item">
           <a class="nav-link" href="#">Link</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
        
       
         <li class="nav-item dropdown">
